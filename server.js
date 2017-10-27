5d31b2c93a56732b8f1ea71781c016b2c51583d7
HuangChongqing <1756260160@qq.com>
2017-10-27 12:06:37 +0800

    创建服务器

---
+++ server.js
@@ -0,0 +1,21 @@
+/*
+* @Author: hasee
+* @Date:   2017-10-27 12:03:37
+* @Last Modified by:   hasee
+* @Last Modified time: 2017-10-27 12:04:12
+*/
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
