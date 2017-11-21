8fba6a8444327a6b9abe675a928d65cef3f95fe4
HuangChongqing <1756260160@qq.com>
2017-11-21 23:01:26 +0800

    router

--- 菜鸟教程/01_router/router.js
+++ 菜鸟教程/01_router/router.js
@@ -1,6 +1,14 @@
 /*
  * @Author: HCQ 
  * @Date: 2017-11-21 22:27:17 
- * @Last Modified by:   HCQ 
- * @Last Modified time: 2017-11-21 22:27:17 
+ * @Last Modified by: HCQ
+ * @Last Modified time: 2017-11-21 22:55:38
  */
+// 我们需要的所有数据都会包含在 request 对象中，该对象作为 onRequest() 回调函数的第一个参数传递。
+// 但是为了解析这些数据，我们需要额外的 Node.JS 模块，它们分别是 url 和 querystring 模块。
+
+function router(pathname) {
+    console.log("About to router a request:", pathname);
+}
+
+exports.router = router;
